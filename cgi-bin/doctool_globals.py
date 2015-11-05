10d09bfbebe93631802a345461249154741d3df4
Albert Visser <albert@lemoncurry>
2015-11-05 23:49:08 +0100

    changed site url

--- cgi-bin/doctool_globals.py
+++ cgi-bin/doctool_globals.py
@@ -3,4 +3,4 @@
 import sys
 progpad = "/home/albert/pythoneer/doctool"
 sys.path.append(progpad) # waar de eigenlijke programmatuur staat
-cgipad = "http://doctool.pythoneer.nl/cgi-bin/"
+cgipad = "http://doctool.lemoncurry.nl/cgi-bin/"
